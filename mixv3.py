7df66b83e7e4ea3f5af6ee9c1d8026aa8de565b4
phucbl <48372474+phucbl@users.noreply.github.com>
2021-03-30 11:50:56 +0700

    Update mixv3.py

--- mixv3.py
+++ mixv3.py
@@ -1,3 +1,4 @@
+import time
 f = open("C:\\Users\\ADMIN\\Desktop\\testcube2801a.gcode","r")
 lines = f.readlines()
 f2 = open("C:\\Users\\ADMIN\\Desktop\\testcube2801b.gcode","r")
@@ -7,6 +8,12 @@ f2.close
 f3 = open("C:\\Users\\ADMIN\\Desktop\\mix1.gcode","w")
 f3.write("")
 f3 = open("C:\\Users\\ADMIN\\Desktop\\mix1.gcode","a")
+#change youself to your value
+retract=5 #retraction amount
+firstlayer = 0.2
+height=0.2
+
+#temp value
 maxz=0
 stringreplace=""
 eValue11=0.00001
@@ -16,19 +23,41 @@ eValue22=eValue21
 eValue13=eValue11
 eValue23=eValue11
 eValueold=eValue11
-height=0.2
 layer=2
 linenum=0   
 linenum2=0
 linenum3=0
-txt1 ="Z"+str(round(height*layer-0.1,1))
-txt2 ="Z"+str(round(height*layer,1))
+zlimit1=0
+zlimit2=0
+zlimit3=0
+zlimit4=0
+if firstlayer==0.2:
+    zlimit1 =round(height*layer-(height/2),1)
+    zlimit2 =round(height*layer,1)
+    zlimit3 =round(height*layer+(height/2),1)
+    zlimit4 =round(height*(layer+1),1)
+else:
+    zlimit1 =round(height*layer,1)
+    zlimit2 =round(height*layer+(height/2),1)
+    zlimit3 =round(height*(layer+1),1)
+    zlimit4 =round(height*(layer+1)+(height/2),1)
+# if firstlayer==0.2:
+#     txt1 ="Z"+str(round(height*layer-(height/2),1))
+#     txt2 ="Z"+str(round(height*layer,1))
+# else:
+#     txt1 ="Z"+str(round(height*layer,1))
+#     txt2 ="Z"+str(round(height*layer+(height/2),1))
+txt1 = "Z"+ str(zlimit1)
+txt2 = "Z"+ str(zlimit2)
+
 
 #find maxZ
-linenum=len(lines)-1
-while lines[linenum].find(" Z")==-1:linenum-=1   
-arrayXYE = lines[linenum].split()
-maxz = float(arrayXYE[4][1:len(arrayXYE[4]):1]) 
+linenum=0
+while lines[linenum].find("MAXZ")==-1:
+    linenum+=1
+arrayXYE = lines[linenum].split(":")
+maxz = float(arrayXYE[1])
+
 
 #copy first layer frome file 0.2
 for linenum in range (0,len(lines)):
@@ -41,13 +70,15 @@ for linenum in range (0,len(lines)):
     else:
         break
 eValueold = eValue11
+# f3.write("---------Endfuntion1Pohuc----------\n")
 
 #find next z in file 0.1
 while lines2[linenum2+3].find(txt1)==-1:
     if (lines2[linenum2].find("E")!=-1 and lines2[linenum2][0]=="G"):
         arrayXYE = lines2[linenum2].split()
-        eValue21 =float(arrayXYE[len(arrayXYE)-1][1:len(arrayXYE[len(arrayXYE)-1]):1]) 
+        eValue21 = float(arrayXYE[len(arrayXYE)-1][1:len(arrayXYE[len(arrayXYE)-1]):1]) 
     linenum2+=1
+
 f3.write(lines2[linenum2]) #position before move Z +0.1
 linenum2+=3
 f3.write(lines2[linenum2]) #Z +0.1 line
@@ -55,15 +86,15 @@ linenum2+=1
 
 #find last Etru Value in file 0.1
 linenum3=linenum2
-while lines2[linenum3].find("E")==-1 or lines2[linenum3][0]!="G":
+while lines2[linenum3].find("E")==-1 or lines2[linenum3][0]!="G" or lines2[linenum3].find("X")==-1:
     linenum3-=1
 arrayXYE = lines2[linenum3].split()    
 indexw = len(arrayXYE)-1
 eValue21= float(arrayXYE[indexw][1:len(arrayXYE[indexw]):1])
 
-#copy file 0.1 , stop at Z0.4
+# copy file 0.1 , stop at Z0.4
 while lines2[linenum2+2].find(txt2)==-1:
-    if lines2[linenum2].find("LAYER")>-1 or lines2[linenum2].find("TIME")>-1 :
+    if  lines2[linenum2].find("TIME")>-1 : #lines2[linenum2].find("LAYER")>-1 or
         linenum2+=1
         continue
     if lines2[linenum2].find("M106")>-1:
@@ -74,22 +105,34 @@ while lines2[linenum2+2].find(txt2)==-1:
         eValue22 = float(arrayXYE[indexx][1:len(arrayXYE[indexx]):1])
         eValue22=round(eValue22-eValue21+eValueold,5)
         stringreplace = lines2[linenum2][0:lines2[linenum2].find(" E")+2:1]
-        stringreplace = stringreplace + str(eValue22) +" ;F1 "+str(linenum2)+ "\n"
+        stringreplace = stringreplace + str(eValue22) + "\n" #+" ;F1 "+str(linenum2)
         f3.write(stringreplace)            
     else: f3.write(lines2[linenum2])
     linenum2+=1
 eValueold=eValue22
-
+# f3.write("---------Endfuntion2Pohuc----------\n")
 ######################
 ######################
 ######################
 
 while(1):
-    if (height*layer>maxz-0.2):break
-    txt2 ="Z"+str(round(height*layer,1))
-    txt3 ="Z"+str(round(height*layer+0.1,1))
-    txt4 ="Z"+str(round(height*(layer+1),1))
+    if (height*layer>maxz-(height*2)):break
+    # if firstlayer==0.2:
+    #     txt2 ="Z"+str(round(height*layer,1))
+    #     txt3 ="Z"+str(round(height*layer+(height/2),1))
+    #     txt4 ="Z"+str(round(height*(layer+1),1))
+    # else: 
+    #     txt2 ="Z"+str(round(height*layer+(height/2),1))
+    #     txt3 ="Z"+str(round(height*(layer+1),1))
+    #     txt4 ="Z"+str(round(height*(layer+1)+(height/2),1))
+    zlimit2+=height
+    zlimit3+=height
+    zlimit4+=height
+    txt2="Z"+str(round(zlimit2,1))
+    txt3="Z"+str(round(zlimit3,1))
+    txt4="Z"+str(round(zlimit4,1))
     txt5 = str(txt2)
+    print (txt2,txt3,txt4,txt5)
     if (txt2[len(txt2)-1]=="0"): txt2 = txt2[0:len(txt2)-2:1]
     if (txt3[len(txt3)-1]=="0"): txt3 = txt3[0:len(txt3)-2:1]
     if (txt4[len(txt4)-1]=="0"): txt4 = txt4[0:len(txt4)-2:1]
@@ -97,14 +140,13 @@ while(1):
     
     
     # if retract
-    print (linenum)
+    if 54950>linenum>54800: print(linenum,lines[linenum])
     if (lines[linenum].find("E")!=-1 and lines[linenum][0]=="G"):
         arrayXYE = lines[linenum].split()
         stringreplace = lines[linenum][0:lines[linenum].find(" E")+2:1]
-        stringreplace = stringreplace + str(eValueold-5)+ "\n"
+        stringreplace = stringreplace + str(eValueold-retract)+ "\n"
         f3.write(stringreplace)
-        eValueold-=5
-        eValue11=eValueold
+        eValueold-=retract
     else:linenum+=1
     linenum+=1
 
@@ -121,14 +163,16 @@ while(1):
 
     #find last Etru Value
     linenum3=linenum
-    while lines[linenum3].find("E")==-1 or lines[linenum3][0]!="G":
+    while lines[linenum3].find("E")==-1 or lines[linenum3][0]!="G" : #or lines[linenum3].find("X")==-1
         linenum3-=1
     arrayXYE = lines[linenum3].split()    
     indexw = len(arrayXYE)-1
     eValue11= float(arrayXYE[indexw][1:len(arrayXYE[indexw]):1])
+    # eValueold=eValue11
 
     #copy all from 0.2 file, stop befor Z0.6, skip outer lines
     while lines[linenum+2].find(txt4)==-1 :
+        if 54950>linenum>54800: print(linenum,lines[linenum])
         if lines[linenum].find(txt4)!=-1 :
             break
         if lines[linenum].find("G92 E0")!=-1 : 
@@ -138,17 +182,25 @@ while(1):
             linenum+=1
             continue
         #when meet outer, find next line type
-        if lines[linenum].find("OUTER")!=-1:
+        if lines[linenum+1].find("OUTER")!=-1:
+            linenum+=2
+            eValueold=eValue12
+            while lines[linenum].find("SKIN")==-1 and lines[linenum].find("FILL")==-1 and lines[linenum].find("INNER")==-1 and lines[linenum][0]=="G": 
+                if (lines[linenum].find("E")!=-1 and lines[linenum][0]=="G" and lines[linenum].find("X")!=-1)  :
+                    arrayXYE = lines[linenum].split()
+                indexx=len(arrayXYE)-1
+                eValue11 = float(arrayXYE[indexx][1:len(arrayXYE[indexx]):1])
                 linenum+=1
-                eValueold=eValue12
-                while lines[linenum].find("SKIN")==-1 and lines[linenum].find("FILL")==-1 and lines[linenum].find("INNER")==-1 and lines[linenum][0]=="G": 
-                    if (lines[linenum].find("E")!=-1 and lines[linenum][0]=="G"):
-                        arrayXYE = lines[linenum].split()
-                    indexx=len(arrayXYE)-1
-                    eValue11 = float(arrayXYE[indexx][1:len(arrayXYE[indexx]):1])
-                    linenum+=1
+           
+            while lines[linenum-1].find("E")==-1 or lines[linenum-1][0]!="G":
                 linenum-=1
-                
+            if 54950>linenum>54800: print(linenum,lines[linenum])
+            while lines[linenum-1].find("X")==-1 or lines[linenum-1].find("E")==-1:
+                linenum-=1
+            while lines[linenum+2].find("Z")!=-1 and lines[linenum+2][0]=="G":
+                linenum+=1
+            # f3.write("------AFTER-OUTER--------\n")
+            if 54950>linenum>54800: print(linenum,lines[linenum])
         if (lines[linenum].find("E")!=-1 and lines[linenum][0]=="G"):
             arrayXYE = lines[linenum].split()
             indexx=len(arrayXYE)-1
@@ -159,35 +211,33 @@ while(1):
             f3.write(stringreplace)
         else: f3.write(lines[linenum])
         linenum+=1
+        if linenum>32000 : 
+            print (linenum)
+            time.sleep(0.5)
     eValueold=eValue12
+    # f3.write("---------Endfuntion3Pohuc----------\n")
 
     #copy 0.4 outer, and 0.5 outer
+    if linenum>32000 : 
+            print (linenum2)
     if (height*layer<=maxz):
         #find last Etru value
         linenum3=linenum2
-        while lines2[linenum3].find("E")==-1 or lines2[linenum3][0]!="G":
+        while lines2[linenum3].find("E")==-1 or lines2[linenum3][0]!="G" or lines2[linenum3].find("X")==-1:
             linenum3-=1
         arrayXYE = lines2[linenum3].split()    
         indexw = len(arrayXYE)-1
         eValue21= float(arrayXYE[indexw][1:len(arrayXYE[indexw]):1])
         #copy 0.4 outer, and 0.5 outer
-        while lines2[linenum2+2].find(txt4)==-1:
+        isZHop = False
+        while lines2[linenum2+2].find(txt4)==-1 or isZHop==True:
             if lines2[linenum2].find("LAYER")>-1 or lines2[linenum2].find("TIME")>-1:
                 linenum2+=1
                 continue
-            #when meet gcode z0.4, skip because we at 0.4 already
-            if lines2[linenum2].find(txt5)>-1:
+            if lines2[linenum2].find(txt5)>-1 and lines2[linenum2-2].find(txt4)==-1:
                 linenum2+=1
                 continue
             
-            # if lines2[linenum2+2].find("SKIN")>-1:
-            #     linenum2+=4
-            #     while lines2[linenum2].find(";")==-1:
-            #         linenum2+=1
-            # if lines2[linenum2].find(txt4)>0:
-            #     break
-
-            if layer>269: print(eValue21,eValue22,eValueold)
             if (lines2[linenum2].find("E")!=-1 and lines2[linenum2][0]=="G"):
                 arrayXYE = lines2[linenum2].split()
                 indexx=len(arrayXYE)-1
@@ -200,7 +250,18 @@ while(1):
             else: 
                 f3.write(lines2[linenum2])
             linenum2+=1
-        print("qwqwqwqw",linenum2,lines2[linenum2])
+            
+            if lines2[linenum2+2].find(txt4)!=-1 and lines2[linenum2+4].find(txt4)!=-1:
+                isZHop=True
+            if lines2[linenum2-1].find(txt4)!=-1 and lines2[linenum2-3].find(txt4)!=-1:
+                isZHop=False
+            if lines2[linenum2+2].find(txt4)!=-1 and lines2[linenum2+3].find(txt4)!=-1:
+                isZHop=True
+            if lines2[linenum2-1].find(txt4)!=-1 and lines2[linenum2-2].find(txt4)!=-1:
+                isZHop=False
+        isZHop = False
+        # f3.write("---------Endfuntion4Pohuc----------\n")
+        # print("qwqwqwqw",linenum2,lines2[linenum2])
     eValueold=eValue22
     #increase layer, back again
     layer+=1
